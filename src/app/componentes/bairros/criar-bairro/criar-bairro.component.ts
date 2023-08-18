f9464a60570887bdd23111deb836540b492818a4
Alisson Jaques - Computer Engineer <alissonjaquesrmq@gmail.com>
2023-08-18 11:12:38 -0300

    Adição de validações no cadastro de bairros

--- src/app/componentes/bairros/criar-bairro/criar-bairro.component.ts
+++ src/app/componentes/bairros/criar-bairro/criar-bairro.component.ts
@@ -7,6 +7,7 @@ import { Municipio } from "../../municipios/interfaces/Municipio";
 import { HttpErrorResponse } from "@angular/common/http";
 import { MatDialog } from "@angular/material/dialog";
 import { openErrorDialog } from "src/app/utils/openErrorDialog";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-criar-bairro",
@@ -14,43 +15,63 @@ import { openErrorDialog } from "src/app/utils/openErrorDialog";
   styleUrls: ["./criar-bairro.component.css"],
 })
 export class CriarBairroComponent implements OnInit {
-  bairro: Bairro = {
-    codigoMunicipio: 0,
-    nome: "",
-    status: 0,
-  };
-
+  formulario!: FormGroup;
   listaMunicipios: Municipio[] = [];
 
   constructor(
     private service: BairroService,
     private municipioService: MunicipioService,
     private router: Router,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit(): void {
     this.municipioService.listar().subscribe((listaMunicipios) => {
       this.listaMunicipios = listaMunicipios;
     });
+    this.formulario = this.formBuilder.group({
+      codigoMunicipio: ["", Validators.compose([Validators.required])],
+      nome: [
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(60),
+          Validators.pattern(/(.|\s)*\S(.|\s)*/),
+        ]),
+      ],
+      status: ["", Validators.compose([Validators.required])],
+    });
   }
 
   criarBairro() {
-    this.bairro.status = Number(this.bairro.status);
-    this.bairro.codigoMunicipio = Number(this.bairro.codigoMunicipio);
-    this.service.criar(this.bairro).subscribe(
-      () => {
-        this.router.navigate(["/bairros"]);
-      },
-      (error: HttpErrorResponse) => {
-        const mensagem = encodeURIComponent(error.error.mensagem);
-        const status = encodeURIComponent(error.error.status);
-        openErrorDialog(this.dialog, mensagem, status);
-      }
-    );
+    if (this.formulario.valid) {
+      this.formulario.value.status = Number(this.formulario.value.status);
+      this.formulario.value.codigoMunicipio = Number(
+        this.formulario.value.codigoMunicipio
+      );
+      this.service.criar(this.formulario.value).subscribe(
+        () => {
+          this.router.navigate(["/bairros"]);
+        },
+        (error: HttpErrorResponse) => {
+          const mensagem = encodeURIComponent(error.error.mensagem);
+          const status = encodeURIComponent(error.error.status);
+          openErrorDialog(this.dialog, mensagem, status);
+        }
+      );
+    }
   }
 
   cancelar() {
     this.router.navigate(["/bairros"]);
+  }
+
+  habilitarBotao(): string {
+    if (this.formulario.valid) {
+      return "botao";
+    }
+    return "botao__desabilitado";
   }
 }
